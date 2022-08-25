80fa5076ae53cff205bd9f58416224c16983daef
alijm0806 <alijm1104@gmail.com>
2022-08-24 20:38:44 -0500

    VUE CRUD

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,20 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Times New Roman', Times, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #060607;
+}
+
+h1 {
+  color: red
 }
 
 nav {
@@ -26,5 +30,31 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#stadium-image {
+  align-items: start;
+  text-align: end;
+}
+
+#create-b {
+  background-color: rgb(65, 167, 193);
+}
+
+#show-b {
+  background-color: rgb(173, 110, 138);
+}
+
+#update-b {
+  background-color: rgb(34, 255, 0);
+}
+
+#delete-b {
+  background-color: red
+}
+
+#stadium-info {
+  align-items: start;
+  text-align: start;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,11 @@ import { assertExpressionStatement } from '@babel/types';
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
-      name: "ALI",
+      message1: "HOST STADIUM",
+      message2: "2026 FIFA WORLD CUP",
       stadiums: [],
-      stadium: {},
-      newStadium: {}
+      newStadium: {},
+      currentStadium: {}
     };
   },
   created: function () { this.indexStadiums() },
@@ -22,29 +22,96 @@ export default {
       })
     },
     showStadium: function (stadium) {
-      console.log("all stadiums..")
-      axios.get("http://localhost:3000/stadiums/4").then(response => {
+      console.log("show stadiums..")
+      axios.get("http://localhost:3000/stadiums/" + stadium.id).then(response => {
         console.log(response.data)
-        this.stadium = response.data
+        this.currentStadium = stadium
+        document.querySelector("#stadium-details").showModal();
       })
     },
+    createStadium: function () {
+      console.log("creating stadiums..");
+      console.log(this.newStadium);
+      axios.post("http://localhost:3000/stadiums", this.newStadium).then(response => {
+        console.log(response.data)
+        this.stadiums.push(this.newStadium)
+
+      })
+    },
+    updateStadium: function (currentStadium) {
+      console.log("updating stadiums..");
+      console.log(this.currentStadium);
+      axios.patch("http://localhost:3000/stadiums/" + this.currentStadium.id, this.currentStadium).then(response => {
+        console.log(this.currentStadium)
+        this.currentStadium = {}
+      })
+
+    },
+    deleteStadium: function (currentStadium) {
+      console.log("deleting stadiums..");
+      console.log(this.currentStadium);
+      axios.delete("http://localhost:3000/stadiums/" + this.currentStadium.id).then(response => {
+        console.log(response.data)
+        var index = this.stadiums.indexOf(currentStadium);
+        console.log(index);
+        // delete that element from the array
+        this.stadiums.splice(index, 1);
+      })
+
+    }
   }
-};
+}
 </script>
 
 <template>
+
   <div class="home">
-    <h1>{{ message }}</h1>
-    <h1>{{ name }}</h1>
+
+    <h1><b>{{ message1 }}</b></h1>
+    <h1><b>{{ message2 }}</b></h1>
+    <hr>
+    <hr>
+
+
+    <div><b>Name: </b><input type="text" v-model="newStadium.name"></div>
+    <div><b>City: </b><input type="text" v-model="newStadium.city"></div>
+    <div><b>address: </b><input type="text" v-model="newStadium.address"></div>
+    <div><b>image: </b><input type="text" v-model="newStadium.image"></div>
+    <button id="create-b" v-on:click="createStadium()">New Stadium</button>
+
+
+
+
+    <hr>
+    <hr>
     <div v-for="stadium in stadiums">
-      <p>{{ stadium.name }}</p>
-      <p>{{ stadium.city }}</p>
-      <br>
-      <button v-on:click="showStadium(stadium)">More Info</button>
+      <p><b>Name: </b>{{ stadium.name }}</p>
+      <p><b>City: </b>{{ stadium.city }}</p>
+      <p><b>Address: </b>{{ stadium.address }}</p>
+      <p><img v-bind:src="stadium.image" width="450" height="250"></p>
+      <button id="show-b" v-on:click="showStadium(stadium)">More Info</button>
+      <hr>
+      <hr>
     </div>
-    <!-- <button v-on:click="indexStadiums()"></button> -->
 
-
+    <dialog id="stadium-details">
+      <form method="dialog">
+        <h1>Stadium info</h1>
+        <p>Name: {{ currentStadium.name }}</p>
+        <p>City: {{ currentStadium.city }}</p>
+        <p>Address: {{ currentStadium.address }}</p>
+        <p>Image: {{ currentStadium.image }}</p>
+        <hr>
+        <hr>
+        <div>Name: <input type="text" v-model="currentStadium.name"></div>
+        <div>City: <input type="text" v-model="currentStadium.city"></div>
+        <div>address: <input type="text" v-model="currentStadium.address"></div>
+        <div>image: <input type="text" v-model="currentStadium.image"></div>
+        <button id="update-b" v-on:click="updateStadium(currentStadium)">Update Stadium</button>
+        <button id="delete-b" v-on:click="deleteStadium(currentStadium)">Delete Stadium</button>
+        <button>Close</button>
+      </form>
+    </dialog>
 
 
   </div>
