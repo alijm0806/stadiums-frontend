423997c4a2addad5503fda40f33586e0234ebd26
alijm0806 <alijm1104@gmail.com>
2022-10-13 19:50:49 -0500

    fix stadiums index

--- src/main.js
+++ src/main.js
@@ -6,9 +6,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'video.js/dist/video-js.css'
-
-
 import Embed from 'v-video-embed'
+
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://mighty-plateau-71758.herokuapp.com";
 
 var jwt = localStorage.getItem("jwt");
