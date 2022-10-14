ff7278ae2652a08c195b66ae94a1deb27271bab0
alijm0806 <alijm1104@gmail.com>
2022-10-13 19:32:44 -0500

    fix stadiums index

--- src/views/StadiumsIndexView.vue
+++ src/views/StadiumsIndexView.vue
@@ -22,7 +22,7 @@ export default {
       map.on('load', () => {
         map.addSource('stadiums', {
           type: 'geojson',
-          data: 'http://localhost:3000/stadiums'
+          data: 'https://mighty-plateau-71758.herokuapp.com/stadiums'
         });
         // Adds layer over the map display corresponding to each post
         map.addLayer({
