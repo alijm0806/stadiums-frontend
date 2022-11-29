8af3738c5c39b30d4ef3cef81c237be09ab28e38
alijm0806 <alijm1104@gmail.com>
2022-11-29 17:54:14 -0600

    Fix data fetching

--- src/views/StadiumsIndexView.vue
+++ src/views/StadiumsIndexView.vue
@@ -22,7 +22,7 @@ export default {
       map.on('load', () => {
         map.addSource('stadiums', {
           type: 'geojson',
-          // data: 'https://mighty-plateau-71758.herokuapp.com/stadiums'
+          data: 'https://mighty-plateau-71758.herokuapp.com/stadiums',
           data: 'http://localhost:3000/stadiums'
         });
 
